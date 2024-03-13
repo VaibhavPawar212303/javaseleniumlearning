be2ba32b8ec04c390cdec31d4d60f9580d25ec90
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2024-03-13 11:19:23 +0530

    added fix for yml

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Working with selenium and java");
+        System.out.println("Hello world!");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://demoqa.com/automation-practice-form");
+        driver.get("http://www.google.com");
     }
-}
+}   
