e9c4731d2025319db69c0ab399f13b8f180fea02
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2024-03-13 11:39:42 +0530

    updated test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,11 +2,16 @@ package com.example;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        WebDriver driver = new ChromeDriver();
+        System.out.println("Hello world! browser open");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless", "false");
+        WebDriver driver = new ChromeDriver(options);
         driver.get("http://www.google.com");
+        driver.close();
+        System.out.println("Hello world! browser close");
     }
 }   
