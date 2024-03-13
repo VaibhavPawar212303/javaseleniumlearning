ff022ea068994e53d73f1c6e4577a2f49ac497ff
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2024-03-13 17:20:37 +0530

    updated bash file

--- generate_workflow.sh
+++ generate_workflow.sh
@@ -1,19 +1,15 @@
 # Check if count argument is provided, default to 5 if not
 count=${1:-5}
 
-cat <<EOF > dynamic_workflow.yml
-name: Dynamic Workflow
+# Loop through the specified count
+for ((i=1; i<=$count; i++)); do
+  cat <<EOF > dynamic_workflow_$i.yml
+name: Dynamic Workflow $i
 on:
   push:
     branches:
-EOF
+      - branch-$i
 
-# Loop through the specified count
-for ((i=1; i<=$count; i++)); do
-  echo "      - branch-$i" >> dynamic_workflow.yml
-done
-
-cat <<EOF >> dynamic_workflow.yml
 jobs:
   test:
     runs-on: ubuntu-latest
