85778ed41f99fcc9ec8888e6cb7a4f37114a514b
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2024-03-13 11:05:31 +0530

    added selenium driver

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,8 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! Working with selenium and java");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/automation-practice-form");
     }
 }
